e53ab2ecb49ff1cc26598ce2f66aed88e4f41c6d
Cory Olson and Matt Wilkin <matt@>
2016-05-31 11:31:18 -0700

    created file directories, js, css, and html files

--- js/script.js
+++ js/script.js
@@ -7,4 +7,18 @@ var lbstokg = function(num1){
   return num1 / .45359;
 };
 
-alert(galtoliter(10));
+//alert(galtoliter(10));
+
+$(function(){
+  $("img").hide();
+  $("#img-1-toggle").click(function(){
+    $("#img-1").toggle();
+  })
+  $("#img-2-toggle").click(function(){
+    $("#img-2").toggle();
+  })
+  $("#img-3-toggle").click(function(){
+    $("#img-3").toggle();
+  })
+
+})
